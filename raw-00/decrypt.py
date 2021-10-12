cb32e2e3a6b9a2fecc975e31e1d37a6ff71ecc84
Marcus Vinícius Ribeiro Andrade <88283829+PyMarcus@users.noreply.github.com>
2021-10-12 18:48:06 -0300

    Update decrypt.py

--- raw-00/decrypt.py
+++ raw-00/decrypt.py
@@ -26,6 +26,20 @@ class Decryptografy:
             with open('readMeFeliz.log', 'a') as f:
                 arquivoLog = f.writelines(f'\nO arquivo {itens} foi descriptografado em {datetime.now()}\n')
                 
+ # voltando para o nome original
+        arqui = itens.split('/')
+        tamanho = len(arqui) - 1
+        nomeDosArquivos = arqui[tamanho]
+        caminhos = itens.split('/')
+        tamanho2 = len(caminhos) - 1
+        novoCaminho = '/'.join(caminhos[:tamanho2]) + '/'
+        os.chdir(novoCaminho)
+        if '.crypt' in nomeDosArquivos:
+            indice = nomeDosArquivos.index('.crypt')
+            nome = nomeDosArquivos[:indice]
+            os.rename(f'./{nomeDosArquivos}', f'./{nome}')  
+                
+                
 
                 
 if __name__ == '__main__':
